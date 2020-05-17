2995dbb55367dc9cf46956161fd673924e59ba5a
PROrock <ondrej.jelinek@email.cz>
2020-05-17 23:51:17 +0200

    go for closest unexplored tile

--- main.py
+++ main.py
@@ -9,13 +9,33 @@ random.seed(0)
 
 WALL = '#'
 SPACE = ' '
+EX_PELLET = '.'
+DIST_TO_ENEMY_ENGAGE = 4
+beats={
+    "ROCK": "SCISSORS",
+    "SCISSORS": "PAPER",
+    "PAPER": "ROCK"
+}
+is_beaten_by={
+    "SCISSORS": "ROCK",
+    "PAPER": "SCISSORS",
+    "ROCK": "PAPER" 
+}
+
 map = []
+# prev_round_pacs = {}
+# my_pacs = {}
 
 class Point:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def l1dist(self, other_point):
+        d = abs(self.x-other_point.x) + abs(self.y-other_point.y)
+        # print(f"l1 dist: {self} and {other_point} is {d}", file=sys.stderr)
+        return d
 
     def __str__(self):
         return f"P({self.x},{self.y})"
@@ -72,15 +92,15 @@ class Node:
                 map_value = mapping(map, new_p)
                 # print(f"new_point is {new_p}, map_val is: {map_value}", file=sys.stderr)        
                 if(map_value != WALL):
-                    new_nodes.append(Node(new_p, self.dist+1, int(map_value)+1 if map_value!=SPACE else -1))
+                    new_nodes.append(Node(new_p, self.dist+1, int(map_value)+1 if map_value in ['0', '9'] else -1))
 
         # print(f"expanded from {self} are: {[str(node) for node in new_nodes]}", file=sys.stderr)        
         return new_nodes
 
 class Search:
-    def __init__(self, start):
+    def __init__(self, start, goals):
         self.start = start
-        # self.map = map
+        self.goals = goals
 
     def search(self):
         front = [Node(self.start, 0, -1)]
@@ -90,7 +110,7 @@ class Search:
             node = front.pop(0) ## take first element -> breadth-first
             # if(node.p not in visited):
             if True: ## todo
-                if(mapping(map, node.p) in ['0', '9']):
+                if(mapping(map, node.p) in self.goals):
                     return node.p
                 new_nodes = node.expand(visited)
                 front.extend(new_nodes)
@@ -103,16 +123,103 @@ class Search:
         return None
         
 class Goal:
-    def __init__(self, p, comment):
-        self.p = p
+    def __init__(self, command, comment):
+        self.command = command
         self.comment = comment
 
-    def __repr__(self):
-        return f"G({self.p},{self.comment})"
+    def to_print(self):
+        return f"{self.command} {self.comment}"
 
+    def __repr__(self):
+        return f"G({self.to_print()})"
+
+def argmax(list):
+    list_val = lambda i: list[i]
+    return max(range(len(list)), key=list_val)
 
 def had_collision(id, prev_round_pacs, my_pacs):
-    return (id in prev_round_pacs and prev_round_pacs[id].p == my_pacs[id].p)
+    b=(id in prev_round_pacs and prev_round_pacs[id].p == my_pacs[id].p)
+    # print(f"collision test id: {id} prev.p={'aaa'} curr.p={'bbb'} bool: {b}", file=sys.stderr)
+    return b
+    
+def move(id, p):
+    return f"MOVE {id} {p.x} {p.y}"
+
+def switch(id, type_id):
+    return f"SWITCH {id} {type_id}"
+
+def speed(id):
+    return f"SPEED {id}"
+
+def random_pellet():
+    return Point(random.randrange(width), random.randrange(height))
+
+def set_goal_with_close_enemy(pac, enemy, dist):
+    type_id = is_beaten_by[enemy.type_id]
+    if pac.type_id != type_id and pac.ability_cooldown==0 and dist <=2:
+        return Goal(switch(pac.id, type_id), f"switching for {enemy}")
+    if pac.type_id == type_id or (pac.ability_cooldown < dist//2 and dist > 2):
+        return  Goal(move(pac.id, enemy.p), f"hunting {enemy}")
+
+    return Goal(move(pac.id, random_pellet()), f"enemy close: {enemy} but dont know what to do")
+
+def set_goal(pac,prev_round_pacs, my_pacs):
+    # hunting/fleeing from CLOSE enemy has priority
+
+    if enemy_pacs:
+        dist_map = {enemy.id: pac.p.l1dist(enemy.p) for enemy in enemy_pacs.values()}
+        # argmax([pac.p.l1dist(enemy.p) for enemy in enemy_pacs.values()])
+        closest_enemy_id = min(dist_map.keys(), key=lambda key: dist_map[key])
+        # for enemy in enemy_pacs.values():
+        #     dist = pac.p.l1dist(enemy.p)
+        dist = dist_map[closest_enemy_id]
+        print(f"closest enemy to {pac} is enemy {enemy_pacs[closest_enemy_id]}, with dist {dist}", file=sys.stderr)
+    else:
+        dist = 1000
+    if dist < DIST_TO_ENEMY_ENGAGE:
+        return set_goal_with_close_enemy(pac, enemy_pacs[closest_enemy_id], dist)
+
+    if had_collision(pac.id, prev_round_pacs, my_pacs):
+        pellet = Point(random.randrange(width), random.randrange(height))
+        return Goal(move(pac.id, pellet), f"coll -> random {pellet}")
+
+    # todo must have next goal more distant than 1!
+    if pac.ability_cooldown == 0:
+        return Goal(speed(pac.id), f"speed!")
+
+    pellet = Search(pac.p, ['0', '9']).search()
+    # closest_pellet_d[pac.id] = pellet
+    if pellet is not None:
+        return Goal(move(pac.id, pellet), f"vis. p. {pellet}")
+
+    # go for closest unexplored space
+    pellet = Search(pac.p, [' ']).search()
+    if pellet is not None:
+        return Goal(move(pac.id, pellet), f"explore {pellet}")
+    else:
+        pellet = Point(random.randrange(width), random.randrange(height))
+        return Goal(move(pac.id, pellet), f"random {pellet}")
+
+# def generate_visible(p):
+
+def generate_ex_pellets(pac):
+    p = pac.p
+    result = set()
+    
+    for grad in [Point(x, y) for x,y in zip([-1, 1, 0, 0], [0, 0, -1, 1])]:
+        # print(f"p={p} grad={grad}", file=sys.stderr)
+        new_p = p
+
+        while(True):
+            new_p = new_p.add(grad)
+            new_val = mapping(map, new_p)
+            # print(f"new_p={new_p} new_val={new_val}", file=sys.stderr)
+
+            if (new_val == WALL):
+                break
+            if (new_val == SPACE):
+                result.add(new_p)
+    return result  
 
 # Grab the pellets as fast as you can!
 
@@ -130,14 +237,13 @@ goals = {}
 
 # game loop
 while True:
-    
+    my_score, opponent_score = [int(i) for i in input().split()]
+
     start_time = time.time()
     map = copy.deepcopy(orig_map)
     copy_time = time.time()
     print(f"copy time {copy_time-start_time}", file=sys.stderr)
 
-
-    my_score, opponent_score = [int(i) for i in input().split()]
 
     prev_round_pacs = my_pacs
     my_pacs = {}
@@ -164,42 +270,54 @@ while True:
         dictionary[pac_id] = pacman
         # print(f"pacman {pacman}", file=sys.stderr)
 
+    # print(f"prev dict {prev_round_pacs}", file=sys.stderr)
+    # print(f"my dict {my_pacs}", file=sys.stderr)
 
     pacs_time = time.time()
     print(f"pacs time {pacs_time-copy_time}", file=sys.stderr)
 
 
     visible_pellet_count = int(input())  # all pellets in sight
+    print(f"visible pellet count {visible_pellet_count}. Range {list(range(visible_pellet_count))}", file=sys.stderr)
+
     for i in range(visible_pellet_count):
         # value: amount of points this pellet is worth
         x, y, value = [int(j) for j in input().split()]
         map[y][x] = str(value-1)
 
-    # for row in map:
-        # print(row, file=sys.stderr)
+    pellet_time = time.time()
+    print(f"pellet time1 {pellet_time-pacs_time}", file=sys.stderr)
+
+
+    # mark empty visible spaces as empty
+    ex_pellets = set()
+    for pac in my_pacs.values():
+        ex_pellets.update(generate_ex_pellets(pac))
+        print(f"ex pellets now {ex_pellets}", file=sys.stderr)
+    
+
+    for ex in ex_pellets:
+        map[ex.y][ex.x] = EX_PELLET
+        orig_map[ex.y][ex.x] = EX_PELLET
+
+
+    for row in map:
+        print(row, file=sys.stderr)
 
     # for id, pac in all_pacs.items():
         # print(f"{id}: {pac}", file=sys.stderr)
+
+    pellet_time = time.time()
+    print(f"pellet time2 {pellet_time-pacs_time}", file=sys.stderr)
 
 
     # todo detect dead pacs if you want nice code without warning
     goals={}
     for pac in my_pacs.values():
-        pellet = Search(pac.p).search()
-        # closest_pellet_d[pac.id] = pellet
-        if pellet is not None and not had_collision(id,prev_round_pacs, my_pacs):
-            goals[pac.id] = Goal(pellet, f"vis. p. {pellet}")
-        elif had_collision(id,prev_round_pacs, my_pacs):
-            pellet = Point(random.randrange(width), random.randrange(height))
-            goals[pac.id] = Goal(pellet, f"coll -> random {pellet}")
-        else:
-            pellet = Point(random.randrange(width), random.randrange(height))
-            goals[pac.id] = Goal(pellet, f"random {pellet}")
+        goals[pac.id] = set_goal(pac,prev_round_pacs, my_pacs)
 
-
-    pellet_time = time.time()
-    print(f"pellet time {pellet_time-pacs_time}", file=sys.stderr)
 
     # MOVE <pacId> <x> <y>
-    s=''.join([f"MOVE {id} {g.p.x} {g.p.y} {g.comment} | " for id, g in goals.items()])
+    # s=''.join([f"MOVE {id} {g.p.x} {g.p.y} {g.comment} | " for id, g in goals.items()])
+    s=' | '.join([g.to_print() for g in goals.values()])
     print(s)
